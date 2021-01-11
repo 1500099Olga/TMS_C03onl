c3deba7b2a83ec490f373f0d4613678b905f10dc
air <1>
2021-01-11 22:16:37 +0300

    ok.

--- src/homework11/Homework11.java
+++ src/homework11/Homework11.java
@@ -1,10 +1,11 @@
 package homework11;
-
 import java.io.*;
 
 public class Homework11 {
     public static void main(String[] args) {
         Lesson1();
+        serialisationCar();
+        deserializationCar();
     }
 
     public static void Lesson1() {
@@ -20,6 +21,26 @@ public class Homework11 {
                 }
             }
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void serialisationCar() {
+        Car car = new Car("Toyota", 210, 22000);
+        System.out.println(car.toString());
+
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("car.dat"))) {
+            oos.writeObject(car);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void deserializationCar() {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("Car.dat"))) {
+            Car car = (Car) ois.readObject();
+            System.out.println(car.toString());
+        } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
